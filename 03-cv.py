b2dbb62cf4d21c10c9e2ce2e3b213bbcfd553f91
Willian Massami Watanabe <watinha@gmail.com>
2026-03-30 14:10:18 -0300

    storing which features were used in each fold validation

--- 03-cv.py
+++ 03-cv.py
@@ -32,19 +32,21 @@ df['mutation_role'] = df['mutation_role'].apply(
 labels = df['mutation_role']
 
 print('dropping columns which will not be used')
-df = df.drop(columns=['mutation_role',
+df = df.drop(columns=['mutation_role', 'target_role',
                       'mutation_xpath', 'target_xpath',
                       'mutation_label', 'target_label',
                       'mutation_className', 'target_className',
                       'mutation_outerHTML', 'target_outerHTML',
                       'hover_img', 'event_img', 'key_img', 'base_img',
                       'mutation_url', 'target_url',
-                      'target_mutation_type', 'mutation_tagName',
+                      #'target_parent_landmark', 'mutation_parent_landmark',
+                      'target_mutation_type',
+                      #'mutation_tagName', 'target_tagName',
                       'target_mutation_attributeName', 'mutation_mutation_attributeName'])
 
 
-string_columns = ['event', 'target_role',
-                  'target_tagName', #'mutation_tagName',
+string_columns = [#'event', 'target_role',
+                  'target_tagName', 'mutation_tagName',
                   'target_parent_landmark', 'mutation_parent_landmark',
                   'mutation_mutation_type']
 
@@ -52,21 +54,31 @@ print('transforming string columns using get_dummies')
 df = pd.get_dummies(df, columns=string_columns)
 
 # FEATURE EXTRACTION
+feature_names = df.columns.tolist()
 X = df.to_numpy()
 y = labels.values
 
-cv_strategy = StratifiedShuffleSplit(n_splits=10)
+cv_strategy = StratifiedShuffleSplit(n_splits=20, random_state=42)
 
-undersampler = ClusterCentroids(sampling_strategy='majority', voting='hard')
+undersampler = ClusterCentroids(sampling_strategy='majority', voting='hard', random_state=42)
 
 print('running CV strategy')
 y_true_all, y_pred_all = [], []
+selected_features_rows = []
 for train_idx, test_idx in cv_strategy.split(X, y, groups):
     X_res, y_res = undersampler.fit_resample(X[train_idx], y[train_idx])
     gridsearch_cv = get_pipeline(classifier_name)
     gridsearch_cv.fit(X_res, y_res)
     y_true_all.extend(y[test_idx])
     y_pred_all.extend(gridsearch_cv.predict(X[test_idx]))
+
+    best = gridsearch_cv.best_estimator_
+    vt_mask = best.named_steps['variance_threshold'].get_support()
+    sel_mask = best.named_steps['selector'].get_support()
+    # Compose: start with variance threshold mask, then apply selector mask within surviving features
+    full_mask = vt_mask.copy()
+    full_mask[vt_mask] = sel_mask
+    selected_features_rows.append(full_mask)
 
 RESULTS_DIR = './03-cv-results'
 RESULTS_PATH = os.path.join(RESULTS_DIR, 'cv_results.xlsx')
@@ -78,6 +90,21 @@ report = classification_report(y_true_all, y_pred_all, digits=4, output_dict=Tru
 report_df = pd.DataFrame(report).T
 
 os.makedirs(RESULTS_DIR, exist_ok=True)
+
+SELECTED_FEATURES_PATH = os.path.join(RESULTS_DIR, 'selected_features.xlsx')
+
+selected_features_df = pd.DataFrame(selected_features_rows, columns=feature_names)
+
+if os.path.isfile(SELECTED_FEATURES_PATH):
+    existing_sf = pd.read_excel(SELECTED_FEATURES_PATH, sheet_name=None, index_col=0)
+else:
+    existing_sf = {}
+
+existing_sf[classifier_name] = selected_features_df
+
+with pd.ExcelWriter(SELECTED_FEATURES_PATH, engine='openpyxl') as writer:
+    for sheet_name, sheet_df in existing_sf.items():
+        sheet_df.to_excel(writer, sheet_name=sheet_name)
 
 if os.path.isfile(RESULTS_PATH):
     existing = pd.read_excel(RESULTS_PATH, sheet_name=None, index_col=0)
